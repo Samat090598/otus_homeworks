e87352a81a954f6110cfab229d78eadbfa3d2c4d
samatmurat <muratsamat090598@gmail.com>
2024-08-11 18:03:18 +0500

    : added logic for unpacking string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,44 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var (
+		lastPos  = -1
+		lastChar string
+		sb       strings.Builder
+	)
+
+	for pos, char := range str {
+		if unicode.IsDigit(char) {
+			if lastPos < 0 {
+				return "", ErrInvalidString
+			}
+
+			count, _ := strconv.Atoi(string(char))
+			sb.WriteString(strings.Repeat(lastChar, count))
+
+			lastPos = -1
+			lastChar = ""
+			continue
+		}
+
+		lastPos = pos
+		if lastChar != "" {
+			sb.WriteString(lastChar)
+		}
+		lastChar = string(char)
+	}
+
+	if lastChar != "" {
+		sb.WriteString(lastChar)
+	}
+
+	return sb.String(), nil
 }
