0e493e3943f09e2c59ad54ff1d1f8276a3367e13
samatmurat <muratsamat090598@gmail.com>
2024-08-17 22:35:53 +0500

    : added logic to the Top10 function

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,52 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type wordInfo struct {
+	word  string
+	count int
+}
+
+func Top10(text string) []string {
+	words := strings.Fields(text)
+	if len(words) < 2 {
+		return words
+	}
+
+	wordFrequencies := make(map[string]int)
+
+	for _, word := range words {
+		if count, found := wordFrequencies[word]; found {
+			wordFrequencies[word] = count + 1
+			continue
+		}
+
+		wordFrequencies[word] = 1
+	}
+
+	wordInfos := make([]wordInfo, 0, len(wordFrequencies))
+	for word, count := range wordFrequencies {
+		wordInfos = append(wordInfos, wordInfo{word: word, count: count})
+	}
+
+	sort.Slice(wordInfos, func(i, j int) bool {
+		if wordInfos[i].count == wordInfos[j].count {
+			return wordInfos[i].word < wordInfos[j].word
+		}
+		return wordInfos[i].count > wordInfos[j].count
+	})
+
+	var result []string
+	for _, word := range wordInfos {
+		result = append(result, word.word)
+	}
+
+	if len(result) < 11 {
+		return result
+	}
+
+	return result[:10]
 }
