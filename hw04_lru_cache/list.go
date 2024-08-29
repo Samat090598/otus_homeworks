e2ffe6dd40a860c70631e135795562673fbeb6fe
samatmurat <muratsamat090598@gmail.com>
2024-08-29 19:26:05 +0500

    : added logic for list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,84 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head   *ListItem
+	tail   *ListItem
+	length int
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.length
+}
+
+func (l *list) Front() *ListItem {
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := ListItem{Value: v}
+
+	if l.head == nil {
+		l.head = &item
+		l.tail = &item
+	} else {
+		item.Next = l.head
+		l.head.Prev = &item
+		l.head = &item
+	}
+
+	l.length++
+	return l.head
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := ListItem{Value: v}
+
+	if l.tail == nil {
+		l.head = &item
+		l.tail = &item
+	} else {
+		item.Prev = l.tail
+		l.tail.Next = &item
+		l.tail = &item
+	}
+
+	l.length++
+	return l.tail
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.tail = i.Prev
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.head = i.Next
+	}
+
+	l.length--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i.Prev == nil {
+		return
+	}
+
+
+	i.Prev.Next = i.Next
+	i.Prev = nil
+	i.Next = l.head
+	l.head.Prev = i
+	l.head = i
 }
