1dd1119a5889be5b72d42d5312f610e6457578a2
samatmurat <muratsamat090598@gmail.com>
2024-09-29 16:33:57 +0500

    : added logic for pipeline

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,32 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	doneWrapper := func(in In) Out {
+		out := make(Bi)
+		go func() {
+			defer close(out)
+			for {
+				select {
+				case <-done: 
+					return
+				case v, ok := <-in: 
+					if !ok { 
+						return
+					}
+					select {
+					case <-done: 
+						return
+					case out <- v:
+					}
+				}
+			}
+		}()
+		return out
+	}
+
+	for _, stage := range stages {
+		in = doneWrapper(stage(in))
+	}
+
+	return in
 }
