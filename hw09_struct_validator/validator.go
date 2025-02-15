f19508622bb10dc23d90bda510e9141c887f5dea
samatmurat <muratsamat090598@gmail.com>
2025-02-16 01:16:41 +0500

    : added validator to int

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -20,19 +20,29 @@ var (
 	ErrInvalidIncomingValue  = errors.New("invalid incoming value, expected structure")
 	ErrInvalidValidationTag  = errors.New("invalid validation tag")
 	ErrIncorrectLengthFormat = errors.New("incorrect length format")
+
+	ErrIncorrectMinFormat = errors.New("incorrect min format")
+	ErrIncorrectMaxFormat = errors.New("incorrect max format")
 )
 
 // Validation errors
 var (
 	ErrIncorrectStrLength  = errors.New("the length of the string does not match the required one")
 	ErrIncorrectStrContent = errors.New("the string does not meet the requirements of the regular expression")
-	ErrIncorrectStr        = errors.New("the string does not match the expected values")
+	ErrUnexpectedValue     = errors.New("the value does not match the expected values")
+
+	ErrMinNotMet = errors.New("the value does not correspond to the min")
+	ErrMaxNotMet = errors.New("the value does not correspond to the max")
 )
 
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
 	panic("implement me")
+}
+
+type validationType interface {
+	string | int
 }
 
 func Validate(v interface{}) error {
@@ -46,17 +56,17 @@ func Validate(v interface{}) error {
 	for i := 0; i < st.NumField(); i++ {
 		field := st.Field(i)
 
-		if validate, ok := field.Tag.Lookup("validate"); ok {
+		if validateTag, ok := field.Tag.Lookup("validate"); ok {
 			var (
 				tp         = field.Type
 				kind       = tp.Kind()
 				reflectVal = reflect.ValueOf(field)
 				fieldName  = field.Name
-				validators = strings.Split(validate, "|")
+				validators = strings.Split(validateTag, "|")
 			)
 
-			if kind == reflect.String {
-				fieldErrors, err := validateString(reflectVal.String(), validators)
+			if kind == reflect.String || kind == reflect.Int {
+				fieldErrors, err := validate(reflectVal.String(), validators)
 				if err != nil {
 					return fmt.Errorf("validate %s value err: %w", fieldName, err)
 				}
@@ -68,30 +78,19 @@ func Validate(v interface{}) error {
 				continue
 			}
 
-			if kind == reflect.Int {
-				//...
-				continue
-			}
-
 			if kind == reflect.Array {
-				if tp.Elem().Kind() == reflect.String {
-					for j := 0; j < reflectVal.Len(); j++ {
-						fieldErrors, err := validateString(reflectVal.Index(j).String(), validators)
-						if err != nil {
-							return fmt.Errorf("validate %s %d elem err: %w", fieldName, j, err)
-						}
-
-						if fieldErrors != nil {
-							validationErrors = append(validationErrors, ValidationError{Field: fieldName})
-						}
+				for j := 0; j < reflectVal.Len(); j++ {
+					fieldErrors, err := validate(reflectVal.Index(j).String(), validators)
+					if err != nil {
+						return fmt.Errorf("validate %s %d elem err: %w", fieldName, j, err)
 					}
 
-					continue
+					if fieldErrors != nil {
+						validationErrors = append(validationErrors, ValidationError{Field: fieldName})
+					}
 				}
 
-				if tp.Elem().Kind() == reflect.Int {
-
-				}
+				continue
 			}
 		}
 
@@ -101,7 +100,7 @@ func Validate(v interface{}) error {
 	return validationErrors
 }
 
-func validateString(value string, validators []string) (error, error) {
+func validate[T validationType](value T, validators []string) (error, error) {
 	var fieldErrors error
 
 	for i := 0; i < len(validators); i++ {
@@ -110,48 +109,97 @@ func validateString(value string, validators []string) (error, error) {
 			return nil, ErrInvalidValidationTag
 		}
 
-		if validator[0] == "len" {
-			length, err := strconv.Atoi(validator[1])
-			if err != nil {
-				return nil, ErrIncorrectLengthFormat
+		switch val := any(value).(type) {
+		case string:
+			if validator[0] == "len" {
+				length, err := strconv.Atoi(validator[1])
+				if err != nil {
+					return nil, ErrIncorrectLengthFormat
+				}
+
+				if utf8.RuneCountInString(val) != length {
+					fieldErrors = fmt.Errorf("%w, %w", fieldErrors, ErrIncorrectStrLength)
+				}
+
+				continue
 			}
 
-			if utf8.RuneCountInString(value) != length {
-				fieldErrors = fmt.Errorf("%w, %w", fieldErrors, ErrIncorrectStrLength)
+			if validator[0] == "regexp" {
+				re, err := regexp.Compile(validator[1])
+				if err != nil {
+					return nil, fmt.Errorf("regexp.Compile err: %w", err)
+				}
+
+				ok := re.MatchString(validator[1])
+				if !ok {
+					fieldErrors = fmt.Errorf("%w, %w", fieldErrors, ErrIncorrectStrContent)
+				}
+
+				continue
 			}
 
-			continue
-		}
+			if validator[0] == "in" {
+				expectedValues := strings.Split(validator[1], ",")
+				var isSuccess bool
+				for j := 0; j < len(expectedValues); j++ {
+					if expectedValues[i] == val {
+						isSuccess = true
+						break
+					}
+				}
 
-		if validator[0] == "regexp" {
-			re, err := regexp.Compile(validator[1])
-			if err != nil {
-				return nil, fmt.Errorf("regexp.Compile err: %w", err)
+				if !isSuccess {
+					fieldErrors = fmt.Errorf("%w, %w", fieldErrors, ErrUnexpectedValue)
+				}
+
+				continue
 			}
+		case int:
+			if validator[0] == "min" {
+				min, err := strconv.Atoi(validator[1])
+				if err != nil {
+					return nil, ErrIncorrectMinFormat
+				}
 
-			ok := re.MatchString(validator[1])
-			if !ok {
-				fieldErrors = fmt.Errorf("%w, %w", fieldErrors, ErrIncorrectStrContent)
-			}
-
-			continue
-		}
-
-		if validator[0] == "in" {
-			expectedValues := strings.Split(validator[1], ",")
-			var isSuccess bool
-			for j := 0; j < len(expectedValues); j++ {
-				if expectedValues[i] == value {
-					isSuccess = true
-					break
+				if val < min {
+					fieldErrors = fmt.Errorf("%w, %w", fieldErrors, ErrMinNotMet)
+					continue
 				}
 			}
 
-			if !isSuccess {
-				fieldErrors = fmt.Errorf("%w, %w", fieldErrors, ErrIncorrectStr)
+			if validator[0] == "max" {
+				min, err := strconv.Atoi(validator[1])
+				if err != nil {
+					return nil, ErrIncorrectMaxFormat
+				}
+
+				if val < min {
+					fieldErrors = fmt.Errorf("%w, %w", fieldErrors, ErrMaxNotMet)
+					continue
+				}
 			}
 
-			continue
+			if validator[0] == "in" {
+				expectedValues := strings.Split(validator[1], ",")
+				var isSuccess bool
+				for j := 0; j < len(expectedValues); j++ {
+					expectedValue, err := strconv.Atoi(expectedValues[i])
+					if err != nil {
+						return nil, fmt.Errorf("parse expected value to int err: %w", err)
+					}
+
+					if expectedValue == val {
+						isSuccess = true
+						break
+					}
+				}
+
+				if !isSuccess {
+					fieldErrors = fmt.Errorf("%w, %w", fieldErrors, ErrUnexpectedValue)
+				}
+
+				continue
+			}
 		}
 	}
 
