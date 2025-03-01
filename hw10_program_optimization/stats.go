f28209d0301aa83cb6bfc125ddccb89236b11969
samatmurat <muratsamat090598@gmail.com>
2025-03-01 14:36:27 +0500

    : replaced io.ReadAll with bufio.Scanner

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,6 +1,7 @@
 package hw10programoptimization
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -31,19 +32,21 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 type users [100_000]User
 
 func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
+	scanner := bufio.NewScanner(r)
+	var user User
+	i := 0
+	for scanner.Scan() {
+		if err = json.Unmarshal(scanner.Bytes(), &user); err != nil {
 			return
 		}
 		result[i] = user
+		i++
 	}
+
+	if err = scanner.Err(); err != nil {
+		return
+	}
+
 	return
 }
 
