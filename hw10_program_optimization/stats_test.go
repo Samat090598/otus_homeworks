ae4251e281aa2b3d055fa6f2a181d3b772ba42ee
samatmurat <muratsamat090598@gmail.com>
2025-03-01 16:47:00 +0500

    : added TestGetUser

--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -38,3 +38,65 @@ func TestGetDomainStat(t *testing.T) {
 		require.Equal(t, DomainStat{}, result)
 	})
 }
+
+func TestGetUser(t *testing.T) {
+	data := `{"ID":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"ID":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"ID":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"ID":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"ID":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+	var u = users{
+		{
+			ID:       1,
+			Name:     "Howard Mendoza",
+			Username: "0Oliver",
+			Email:    "aliquid_qui_ea@Browsedrive.gov",
+			Phone:    "6-866-899-36-79",
+			Password: "InAQJvsq",
+			Address:  "Blackbird Place 25",
+		},
+		{
+			ID:       2,
+			Name:     "Jesse Vasquez",
+			Username: "qRichardson",
+			Email:    "mLynch@broWsecat.com",
+			Phone:    "9-373-949-64-00",
+			Password: "SiZLeNSGn",
+			Address:  "Fulton Hill 80",
+		},
+		{
+			ID:       3,
+			Name:     "Clarence Olson",
+			Username: "RachelAdams",
+			Email:    "RoseSmith@Browsecat.com",
+			Phone:    "988-48-97",
+			Password: "71kuz3gA5w",
+			Address:  "Monterey Park 39",
+		},
+		{
+			ID:       4,
+			Name:     "Gregory Reid",
+			Username: "tButler",
+			Email:    "5Moore@Teklist.net",
+			Phone:    "520-04-16",
+			Password: "r639qLNu",
+			Address:  "Sunfield Park 20",
+		},
+		{
+			ID:       5,
+			Name:     "Janice Rose",
+			Username: "KeithHart",
+			Email:    "nulla@Linktype.com",
+			Phone:    "146-91-01",
+			Password: "acSBF5",
+			Address:  "Russell Trail 61",
+		},
+	}
+
+	t.Run("parse data", func(t *testing.T) {
+		result, err := getUsers(bytes.NewBufferString(data))
+		require.NoError(t, err)
+		require.Equal(t, u, result)
+	})
+}
