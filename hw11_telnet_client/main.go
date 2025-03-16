b8d31c025c28b9f25f4f69c7e1d7516f6d2673e9
samatmurat <muratsamat090598@gmail.com>
2025-03-16 19:51:21 +0500

    : added close

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -47,6 +47,7 @@ func main() {
 	go func() {
 		errCh <- client.Receive()
 	}()
+	close(errCh)
 
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
